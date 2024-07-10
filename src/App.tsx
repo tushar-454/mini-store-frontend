ff92435ca05ab3ef15474312cf696252cd34555c
tushar-454 <imtushar454@gmail.com>
2024-07-10 10:52:07 +0600

    chore: Add initial project files and dependencies

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import { RouterProvider } from 'react-router-dom';
+import router from './Routes/Routes';
+
+const App = () => {
+  return <RouterProvider router={router} />;
+};
+
+export default App;
---
+++ src/layout/Root.tsx
@@ -0,0 +1,11 @@
+const Root = () => {
+  return (
+    <div>
+      <h1>header</h1>
+      <h1>children</h1>
+      <h1>footer</h1>
+    </div>
+  );
+};
+
+export default Root;
