b2326afb2afbd8a2e9349ab77bf6bd62dd1ea769
tushar-454 <imtushar454@gmail.com>
2024-07-10 11:44:28 +0600

    chore: Header component added desktop mode when user logged in

--- src/components/header/Logo.tsx
+++ src/components/header/Logo.tsx
@@ -1,10 +1,13 @@
+import { Link } from 'react-router-dom';
 import logo from '../../assets/logo.webp';
 
 const Logo = () => {
   return (
-    <div className='flex items-center gap-2'>
-      <img src={logo} alt='mini store logo' className='w-20' />
-      <span className='text-3xl font-black text-green-600'>MS</span>
+    <div>
+      <Link to={'/'} className='flex items-center gap-2'>
+        <img src={logo} alt='mini store logo' className='w-20' />
+        <span className='text-3xl font-black text-green-600'>MS</span>
+      </Link>
     </div>
   );
 };
--- src/components/header/UserAction.tsx
+++ src/components/header/UserAction.tsx
@@ -1,10 +1,42 @@
+import { CiHeart, CiSearch, CiShoppingCart, CiUser } from 'react-icons/ci';
+import { Link } from 'react-router-dom';
 import LinkBtn from '../shared/LinkBtn';
 
 const UserAction = () => {
+  const userLoggedIn = true;
   return (
-    <div>
-      <LinkBtn>Login / Signup</LinkBtn>
-    </div>
+    <>
+      {userLoggedIn ? (
+        <div>
+          <ul className='flex items-center gap-4'>
+            <li>
+              <Link to={'/'} className='text-2xl'>
+                <CiSearch />
+              </Link>
+            </li>
+            <li>
+              <Link to={'/'} className='text-2xl'>
+                <CiShoppingCart />
+              </Link>
+            </li>
+            <li>
+              <Link to={'/'} className='text-2xl'>
+                <CiHeart />
+              </Link>
+            </li>
+            <li>
+              <Link to={'/'} className='text-2xl'>
+                <CiUser />
+              </Link>
+            </li>
+          </ul>
+        </div>
+      ) : (
+        <div>
+          <LinkBtn>Login / Signup</LinkBtn>
+        </div>
+      )}
+    </>
   );
 };
 
