11154a009fcfae937967cc4ca86506731913946e
tushar-454 <imtushar454@gmail.com>
2024-07-12 10:07:58 +0600

    feat: Add order confirmation image to OrderConfirm component

--- src/components/OrderSummary/OrderConfirm.tsx
+++ src/components/OrderSummary/OrderConfirm.tsx
@@ -1,5 +1,11 @@
+import orderCompleted from '../../assets/order-completed.webp';
+
 const OrderConfirm = () => {
-  return <div>order confirm</div>;
+  return (
+    <div className='grid place-items-center'>
+      <img src={orderCompleted} alt='' />
+    </div>
+  );
 };
 
 export default OrderConfirm;
