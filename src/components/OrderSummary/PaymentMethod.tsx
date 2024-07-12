ed41e3a264e2d141c8fd622745ec7fb5f4d16de9
tushar-454 <imtushar454@gmail.com>
2024-07-12 09:31:05 +0600

    refactor: Update PaymentMethod component to include payment type options and styling improvements

--- src/components/OrderSummary/PaymentMethod.tsx
+++ src/components/OrderSummary/PaymentMethod.tsx
@@ -1,5 +1,40 @@
+import paymentway from '../../assets/payment-way.webp';
 const PaymentMethod = () => {
-  return <div>PaymentMethod</div>;
+  return (
+    <>
+      <div>
+        <h2 className='text-2xl font-semibold'>Payment Method</h2>
+        <p className='text-lg text-gray-500'>
+          Please enter your payment method
+        </p>
+      </div>
+      {/* pay type  */}
+      <div className='mt-10 flex gap-5'>
+        <div className='flex items-center gap-2'>
+          <input
+            type='radio'
+            name='payType'
+            id='payOnline'
+            className='accent-green-600'
+          />
+          <label htmlFor='payOnline'> Pay Online</label>
+        </div>
+
+        <div className='flex items-center gap-2'>
+          <input
+            type='radio'
+            name='payType'
+            id='cod'
+            className='accent-green-600'
+          />
+          <label htmlFor='cod'> Cash On Delivery</label>
+        </div>
+      </div>
+      <div>
+        <img src={paymentway} alt='' className='w-96 cursor-pointer' />
+      </div>
+    </>
+  );
 };
 
 export default PaymentMethod;
