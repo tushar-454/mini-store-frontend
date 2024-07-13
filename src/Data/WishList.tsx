f6206d70476adf2d1fbc74e24bbec50c8f97f3f0
tushar-454 <imtushar454@gmail.com>
2024-07-13 12:34:56 +0600

    feat: Add WishList data and update Wishlists page

---
+++ src/Data/WishList.tsx
@@ -0,0 +1,36 @@
+import { productType } from './FeaturedProducts';
+
+const Wishlists: productType[] = [
+  {
+    img: 'https://i.ibb.co/0s3pdnc/4.jpg',
+    title: 'Men T-shirt',
+    price: 25,
+    salePrice: 20,
+  },
+  {
+    img: 'https://i.ibb.co/0s3pdnc/4.jpg',
+    title: 'Women Pant',
+    price: 25,
+    salePrice: 20,
+  },
+  {
+    img: 'https://i.ibb.co/0s3pdnc/4.jpg',
+    title: 'Women Bag',
+    price: 25,
+    salePrice: 20,
+  },
+  {
+    img: 'https://i.ibb.co/0s3pdnc/4.jpg',
+    title: 'Children Toy',
+    price: 25,
+    salePrice: 20,
+  },
+  {
+    img: 'https://i.ibb.co/0s3pdnc/4.jpg',
+    title: 'Camera',
+    price: 25,
+    salePrice: 20,
+  },
+];
+
+export default Wishlists;
--- src/pages/Wishlists.tsx
+++ src/pages/Wishlists.tsx
@@ -1,3 +1,4 @@
+import WishlistsArr from '../Data/WishList';
 import FeaturedProductCard from '../components/FeaturedProducts/FeaturedProductCard';
 import Breadcrumb from '../components/shared/Breadcrumb';
 import Container from '../components/shared/Container';
@@ -17,8 +18,8 @@ const Wishlists = () => {
         </div>
         {/* wishlists products  */}
         <div className='grid grid-cols-1 justify-between gap-5 pb-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'>
-          {[{}, {}, {}, {}, {}, {}, {}, {}].map((_, index) => (
-            <FeaturedProductCard key={index} />
+          {WishlistsArr.map((product) => (
+            <FeaturedProductCard key={Math.random()} product={product} />
           ))}
         </div>
       </Container>
