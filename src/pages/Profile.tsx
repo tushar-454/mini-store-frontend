aaf899d5531218221db6cf6646a7f76657884694
tushar-454 <imtushar454@gmail.com>
2024-07-18 16:28:14 +0600

    feat: Add toggle functionality to user profile sidebar
    The user profile sidebar in the Profile page now has a toggle functionality. Clicking on the arrow icon expands or collapses the sidebar, providing a better user experience. This change improves the usability of the profile page.

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,17 +1,26 @@
+import { useState } from 'react';
 import toast from 'react-hot-toast';
+import { MdArrowForwardIos } from 'react-icons/md';
 import { NavLink, Outlet } from 'react-router-dom';
 import Container from '../components/shared/Container';
 import useAuth from '../Hook/useAuth';
 
 const Profile = () => {
   const { logout, user } = useAuth();
+  const [isAsideOpen, setIsAsideOpen] = useState(false);
   return (
     <section>
       <Container>
         <div className='flex'>
-          <aside className='h-screen w-[300px] border-x'>
+          <aside
+            className={`absolute z-50 h-screen w-[300px] border-x bg-white transition-all md:relative md:z-auto ${isAsideOpen ? 'left-0' : 'left-[-19rem] md:left-0'}`}
+          >
             {/* user information  */}
-            <div className='flex items-center gap-5 p-3'>
+            <div className='relative flex items-center gap-5 p-3'>
+              <MdArrowForwardIos
+                onClick={() => setIsAsideOpen(!isAsideOpen)}
+                className={`absolute -right-10 top-0 block h-10 w-10 bg-green-600 p-2 text-white md:hidden ${isAsideOpen ? 'rotate-180 rounded-l-full' : 'rotate-0 rounded-r-full'}`}
+              />
               <img
                 src={
                   user?.photoURL ??
@@ -29,24 +38,28 @@ const Profile = () => {
             </div>
             <div className='mt-10 grid'>
               <NavLink
+                onClick={() => setIsAsideOpen(false)}
                 to={'/profile'}
                 className='mb-2 cursor-pointer p-2 px-3 text-lg font-semibold hover:bg-neutral-100'
               >
                 Profile
               </NavLink>
               <NavLink
+                onClick={() => setIsAsideOpen(false)}
                 to={'/profile/shipping'}
                 className='mb-2 cursor-pointer p-2 px-3 text-lg font-semibold hover:bg-neutral-100'
               >
                 Shipping
               </NavLink>
               <NavLink
+                onClick={() => setIsAsideOpen(false)}
                 to={'/profile/order'}
                 className='mb-2 cursor-pointer p-2 px-3 text-lg font-semibold hover:bg-neutral-100'
               >
                 Order
               </NavLink>
               <NavLink
+                onClick={() => setIsAsideOpen(false)}
                 to={'/profile/review'}
                 className='mb-2 cursor-pointer p-2 px-3 text-lg font-semibold hover:bg-neutral-100'
               >
@@ -57,6 +70,7 @@ const Profile = () => {
                 onClick={() => {
                   logout();
                   toast.success('Logout Successfully');
+                  setIsAsideOpen(false);
                 }}
                 className='mb-2 cursor-pointer p-2 px-3 text-lg font-semibold hover:bg-neutral-100'
               >
