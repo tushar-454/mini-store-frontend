a9f7fc76ef4bd4cd5201c2b922f4db8da0352ec4
tushar-454 <imtushar454@gmail.com>
2024-07-18 15:54:48 +0600

    refactor: user profile section user information show

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -1,8 +1,23 @@
+import useAuth from '../../Hook/useAuth';
+import useUserInfo from '../../Hook/useUserInfo';
+
 const Profile = () => {
+  const { user } = useAuth();
+  const { userInfo } = useUserInfo();
   return (
-    <div>
-      <h1>Profile</h1>
-    </div>
+    <section>
+      <div className='space-y-5'>
+        <p className='text-3xl font-bold'>Name: {user?.displayName}</p>
+        <p className='text-2xl font-semibold'>
+          Email: {user?.email} [
+          {user?.emailVerified ? 'Verified' : 'Not Verified'}]
+        </p>
+        <p className='text-xl font-medium'>
+          Phone: {userInfo?.data.phone ?? 'null'}
+        </p>
+        <p className='text-lg'>Account create: {user?.metadata.creationTime}</p>
+      </div>
+    </section>
   );
 };
 
