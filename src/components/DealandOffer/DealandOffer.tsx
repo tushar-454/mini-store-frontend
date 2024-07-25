c23b0dc6f73efef6cc69a861edb2a7e4a737cc92
tushar-454 <imtushar454@gmail.com>
2024-07-25 22:40:02 +0600

    deal section some product added

--- src/components/DealandOffer/DealandOffer.tsx
+++ src/components/DealandOffer/DealandOffer.tsx
@@ -1,3 +1,6 @@
+import DealsAndOffer, {
+  DealsAndOfferProductType,
+} from '../../Data/DealsAndOffer';
 import Container from '../shared/Container';
 
 const DealandOffer = () => {
@@ -32,24 +35,27 @@ const DealandOffer = () => {
           </div>
           {/* offer products  */}
           <div className='flex flex-wrap gap-5'>
-            {[{}, {}, {}, {}, {}].map((_, inx) => (
+            {DealsAndOffer?.map((offerItem: DealsAndOfferProductType) => (
               <div
-                key={inx}
+                key={Math.random()}
                 className='grid w-40 flex-grow basis-[200px] gap-1 rounded-lg bg-neutral-50 p-2 xl:flex-grow xl:basis-auto'
               >
                 <div className='relative mb-2'>
                   <img
-                    src='https://static-01.daraz.com.bd/p/d64a3d50a2df4ae391cdade5aee26b04.jpg_750x400.jpg_.webp'
+                    src={offerItem.image}
                     alt='product image'
                     className='w-full rounded-lg object-cover'
                   />
                   <small className='absolute left-1 top-1 grid h-6 w-12 place-items-center rounded-full bg-red-100 font-bold text-red-500'>
-                    20%
+                    {offerItem.discount}%
                   </small>
                 </div>
-                <span className='font-medium'>Hand Bag</span>
+                <span className='font-medium'>{offerItem.name}</span>
                 <span>
-                  Price: <b className='text-lg'>$320</b>
+                  Price:{' '}
+                  <b className='text-lg'>
+                    ${Math.floor((offerItem.price * offerItem.discount) / 100)}
+                  </b>
                 </span>
               </div>
             ))}
